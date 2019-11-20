6abed6fdac77a3df66c3850ab945c241053b1da7
Martin Levesque <levesque.martin@gmail.com>
2019-11-19 20:47:14 -0500

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,9 @@
 
-echo "done"
+sshpass -p $API_PASSWORD ssh -o StrictHostKeyChecking=no $API_USER@$API_HOST \
+  "PATH=\"$PATH:$RUBY_BIN_PATH\" && \
+  cd $PROJECT_PATH && \
+  echo 'CWD:' && pwd && \
+  echo 'git pulling' && git pull && \
+  echo 'bundle install' && ./bin/bundle install && \
+  RAILS_ENV=$RAILS_ENV ./bin/rails db:migrate && \
+  pm2 list" # replace with reload
