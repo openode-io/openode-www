ad08bf1fbcb092e71a313b8e36f9731e42b0ac2b
Gaston Soto <gallzeus@gmail.com>
2019-12-20 15:17:37 -0300

    home page first iteration

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import '../stylesheets/application'
+import './bootstrap_custom.js'
+import './utils.js'
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
