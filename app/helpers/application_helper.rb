a75bdfed80eb0f741ce00f06d47ebcc8c1fb4355
Gaston Soto <gallzeus@gmail.com>
2019-12-26 15:56:16 -0300

    tests update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+
+  def title
+    if content_for?(:title)
+      content_for :title
+    else
+      t("#{ controller_path.tr('/', '.') }.#{ action_name }.title", default: :site_name)
+    end
+  end
+
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -5,76 +5,76 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     get '/'
 
     assert_response :success
-    assert_includes response.parsed_body, "| opeNode</title>"
+    assert_includes response.parsed_body, "| Home</title>"
   end
 
   test "should get pricing" do
     get '/pricing'
 
     assert_response :success
-    assert_includes response.parsed_body, "Home#pricing"
+    assert_includes response.parsed_body, "| Pricing</title>"
   end
 
   test "should get private_cloud_pricing" do
     get '/pricing-private-cloud'
 
     assert_response :success
-    assert_includes response.parsed_body, "Home#private_cloud_pricing"
+    assert_includes response.parsed_body, "| Pricing</title>"
   end
 
   test "should get about" do
     get '/about'
 
     assert_response :success
-    assert_includes response.parsed_body, "Home#about"
+    assert_includes response.parsed_body, "| About Us</title>"
   end
 
   test "should get news" do
     get '/news'
 
     assert_response :success
-    assert_includes response.parsed_body, "Home#news"
+    assert_includes response.parsed_body, "| News</title>"
   end
 
   test "should get locations" do
     get '/locations'
 
     assert_response :success
-    assert_includes response.parsed_body, "Home#locations"
+    assert_includes response.parsed_body, "| Locations</title>"
   end
 
   test "should get openode cli" do
     get '/openode-cli'
 
     assert_response :success
-    assert_includes response.parsed_body, "Home#openode_cli"
+    assert_includes response.parsed_body, "| CLI</title>"
   end
 
   test "should get faq" do
     get '/faq'
 
     assert_response :success
-    assert_includes response.parsed_body, "Home#faq"
+    assert_includes response.parsed_body, "| FAQ</title>"
   end
 
   test "should get support" do
     get '/support'
 
     assert_response :success
-    assert_includes response.parsed_body, "Home#support"
+    assert_includes response.parsed_body, "| Support</title>"
   end
 
   test "should get terms" do
     get '/terms'
 
     assert_response :success
-    assert_includes response.parsed_body, "Home#terms"
+    assert_includes response.parsed_body, "| Terms</title>"
   end
 
   test "should get privacy" do
     get '/privacy'
 
     assert_response :success
-    assert_includes response.parsed_body, "Home#privacy"
+    assert_includes response.parsed_body, "| Privacy</title>"
   end
 end
