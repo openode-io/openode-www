842178cf18390e70c15365eb48224d42c170ac99
Gaston Soto <gallzeus@gmail.com>
2020-01-06 11:31:32 -0300

    login page and alert styling using bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,4 +27,13 @@ module ApplicationHelper
     
     data.as_json.deep_symbolize_keys                                                               
   end  
+
+  def flash_bootstrap(level)
+    case level
+        when 'notice' then "alert alert-info"
+        when 'success' then "alert alert-success"
+        when 'error' then "alert alert-danger"
+        when 'alert' then "alert alert-danger"
+    end
+end  
 end
