7fd2fc4bacf60dde23e614e4d7f778d5b0a11a1f
Gaston Soto <gallzeus@gmail.com>
2020-01-06 12:03:39 -0300

    rubocop fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,31 +9,32 @@ module ApplicationHelper
     end
   end
 
-  def directory_hash(path, name=nil, exclude = [])                                
-    exclude.concat(['..', '.', '.git', '__MACOSX', '.DS_Store'])   
+  def directory_hash(path, name = nil, exclude = [])
+    exclude.concat(['..', '.', '.git', '__MACOSX', '.DS_Store'])
 
-    data = {:text => (name || path)}                                             
-    data[:children] = children = []                                               
+    data = { text: (name || path) }
+    data[:children] = children = []
 
-    Dir.foreach(path) do |entry|                                                  
-      next if exclude.include?(entry) || entry.downcase.include?('.json')                                       
-      full_path = File.join(path, entry)                                          
-      if File.directory?(full_path)                                               
-        children << directory_hash(full_path, entry)                              
-      else                                                                        
-        children << {:icon => '', :text => entry.gsub(/.md|.mdx/,'')}    
-      end                                                                         
-    end                                                                           
-    
-    data.as_json.deep_symbolize_keys                                                               
-  end  
+    Dir.foreach(path) do |entry|
+      next if exclude.include?(entry) || entry.downcase.include?('.json')
+
+      full_path = File.join(path, entry)
+      children << if File.directory?(full_path)
+                    directory_hash(full_path, entry)
+                  else
+                    { icon: '', text: entry.gsub(/.md|.mdx/, '') }
+                  end
+    end
+
+    data.as_json.deep_symbolize_keys
+  end
 
   def flash_bootstrap(level)
     case level
-        when 'notice' then "alert alert-info"
-        when 'success' then "alert alert-success"
-        when 'error' then "alert alert-danger"
-        when 'alert' then "alert alert-danger"
+    when 'notice' then "alert alert-info"
+    when 'success' then "alert alert-success"
+    when 'error' then "alert alert-danger"
+    when 'alert' then "alert alert-danger"
     end
-  end  
+  end
 end
