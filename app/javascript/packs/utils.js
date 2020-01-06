4aaa8f4b671a263954bf2ab28956bb4c62758fe8
Gaston Soto <gallzeus@gmail.com>
2020-01-06 15:23:13 -0300

    twitter feed updates

--- app/javascript/packs/utils.js
+++ app/javascript/packs/utils.js
@@ -1,21 +1,7 @@
 import Typed from 'typed.js'
 
-export  var customizeTweetMedia = function () {
-  // CSS Overrides
-  $('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-Tweet-media').css('display', 'none');
-  $('.twitter-feed').find('.twitter-timeline').contents().find('span.TweetAuthor-screenName').css('font-size', '16px');
-  $('.twitter-feed').find('.twitter-timeline').contents().find('p.timeline-tweet-text').css('font-size', '20px');
-  $('.twitter-feed').find('.twitter-timeline').contents().find('p.timeline-tweet-text').css('line-height', '1.6');
-  $('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-TweetList-tweet').css({'border-bottom':'1px solid #252829','padding-bottom':'20px','margin-bottom':'20px'});
-
-  // Call the function on dynamic updates in addition to page load
-  $('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-TweetList').bind('DOMSubtreeModified propertychange', function () {
-      customizeTweetMedia(this);
-  });
-}
-
 document.addEventListener("turbolinks:load", function() {
-  const el = document.querySelector('.typed');
+  const el = document.querySelector('.typed');  
 
   if (el){
     new Typed('.typed', {
@@ -24,8 +10,14 @@ document.addEventListener("turbolinks:load", function() {
     });
   }
 
-  // Twitter Overrides -----------------------------------------------------------------------------
-  $('.twitter-feed').delegate('#twitter-widget-0', 'DOMSubtreeModified propertychange', function () {
-    customizeTweetMedia();
-  });  
+  if ($("#twitter-widget-0")) {
+    // Twitter Overrides -----------------------------------------------------------------------------
+    $('.twitter-feed').delegate('#twitter-widget-0', 'DOMSubtreeModified propertychange', function () {
+      $('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-Tweet-media').css('display', 'none');
+      $('.twitter-feed').find('.twitter-timeline').contents().find('span.TweetAuthor-screenName').css('font-size', '16px');
+      $('.twitter-feed').find('.twitter-timeline').contents().find('p.timeline-tweet-text').css('font-size', '20px');
+      $('.twitter-feed').find('.twitter-timeline').contents().find('p.timeline-tweet-text').css('line-height', '1.6');
+      $('.twitter-feed').find('.twitter-timeline').contents().find('.timeline-Tweet').css({'border-bottom':'1px solid #252829','padding-bottom':'20px','margin-bottom':'20px'});
+    }); 
+  } 
 })
