a7166849e0f280c10954047893b59b1264dffceb
Gaston Soto <gallzeus@gmail.com>
2020-01-09 17:05:03 -0300

    small updates

--- app/controllers/concerns/recaptchable.rb
+++ app/controllers/concerns/recaptchable.rb
@@ -3,6 +3,8 @@ module Recaptchable
 
   included do
     def verify_recaptchas
+      return true if Rails.env.test?
+
       v2_secret_key = Rails.application.credentials.recaptcha_v2[:secret_key]
       v3_secret_key = Rails.application.credentials.recaptcha_v3[:secret_key]
 
@@ -16,7 +18,7 @@ module Recaptchable
         checkbox_success = verify_recaptcha(secret_key: v2_secret_key)
       end
 
-      @show_checkbox_recaptcha = !success
+      @show_recaptcha_v2 = !success
 
       (success || checkbox_success)
     end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,8 @@
 module UsersHelper
-  def render_recaptcha(action, show_checkbox_recaptcha)
-    if show_checkbox_recaptcha
+  def render_recaptcha(action, show_recaptcha_v2)
+    return if Rails.env.test?
+
+    if show_recaptcha_v2
       recaptcha_tags(
         site_key: Rails.application.credentials.recaptcha_v2[:site_key]
       )
