586e54f78118467523abd3e123a9d14511c5d95f
Martin Levesque <levesque.martin@gmail.com>
2020-01-30 20:10:46 -0500

    test auto deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,14 @@
-
-sshpass -p $API_PASSWORD ssh -o StrictHostKeyChecking=no $API_USER@$API_HOST \
+echo $SERVER_PK_KEY | base64 --decode > id_rsa_tmp
+ssh -i id_rsa_tmp -o StrictHostKeyChecking=no $SERVER_USER@$SERVER_HOST \
   "PATH=\"$PATH:$RUBY_BIN_PATH\" && \
   cd $PROJECT_PATH && \
   echo 'CWD:' && pwd && \
   echo 'git pulling' && git pull && \
   echo 'bundle install' && bundle install && \
+  echo 'yarn' && yarn && \
+  echo 'rails assets:precompile' && RAILS_ENV=$RAILS_ENV rails assets:precompile && \
   RAILS_ENV=$RAILS_ENV ./bin/rails db:migrate && \
+  pm2 restart openode-www && \
   pm2 list" # replace with reload
+
+rm -f id_rsa_tmp
