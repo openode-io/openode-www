58eb2dc13b51c69dfcde1f2eae1e5887c87294e8
Martin Levesque <levesque.martin@gmail.com>
2020-02-18 20:45:55 -0500

    fix

--- app/helpers/admin/instance_access_helper.rb
+++
@@ -1,5 +0,0 @@
-module Admin::InstanceAccessHelper
-  def instance_access_to(instance_id)
-    "#{instance_to(instance_id)}access/"
-  end
-end
--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -6,4 +6,8 @@ module AdminHelper
   def instance_to(instance_id)
     "/admin/instance/#{instance_id}/"
   end
+
+  def instance_access_to(instance_id)
+    "#{instance_to(instance_id)}access/"
+  end
 end
