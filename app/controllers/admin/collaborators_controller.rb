ca565788c5c527a354af9125479c9826d4b7dafe
Martin Levesque <levesque.martin@gmail.com>
2020-02-20 21:10:01 -0500

    fix test

--- app/controllers/admin/collaborators_controller.rb
+++ app/controllers/admin/collaborators_controller.rb
@@ -1,6 +1,5 @@
 class Admin::CollaboratorsController < Admin::InstancesController
   def index
     @collaborators = api(:get, "/instances/#{@instance_id}/collaborators")
-    puts "colls #{@collaborators.to_json}"
   end
 end
