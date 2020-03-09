7c909b840a5bc18493ea809d8f8ad6847d486187
Martin Levesque <levesque.martin@gmail.com>
2020-03-08 21:40:12 -0400

    fix

--- test/http_stubs.rb
+++ test/http_stubs.rb
@@ -574,7 +574,7 @@ module HttpStubs
       HttpStubs.default_post('https://api.openode.io/instances/152/restart',
                              {},
                              'test/fixtures/http/openode_api/empty_object.json',
-                             logged_in_user_token),
+                             logged_in_user_token)
     ]
   end
 end
