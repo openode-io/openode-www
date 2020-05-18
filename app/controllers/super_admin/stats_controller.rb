d7322de237d8ecff01c0719e0d86a195266b23fe
Martin Levesque <levesque.martin@gmail.com>
2020-05-17 20:13:26 -0400

    add stats nb online

--- app/controllers/super_admin/stats_controller.rb
+++ app/controllers/super_admin/stats_controller.rb
@@ -1,5 +1,6 @@
 class SuperAdmin::StatsController < SuperAdminController
   def index
     @spendings = api(:get, "/super_admin/stats/spendings")
+    @nb_online_stats = api(:get, "/super_admin/stats/nb_online")
   end
 end
